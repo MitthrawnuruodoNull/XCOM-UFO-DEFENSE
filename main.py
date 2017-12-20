5a4772521a842398199d9b993c07f2dd24994f97
MitthrawnuruodoNull <34247396+MitthrawnuruodoNull@users.noreply.github.com>
2017-12-20 10:04:29 -0500

    Updating Python
    More crap

--- main.py
+++ main.py
@@ -2,4 +2,19 @@
 import pygame
 import random
 #THERE MAY BE MORE LATER
+pygame.init()
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('X-COM UFO DEFENSE')
+done = False
+while done == False:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          done = True
+          pygame.quit()
+        
+          
+  
 
+    
