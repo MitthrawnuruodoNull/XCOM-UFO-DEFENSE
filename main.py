26f749d23309f455b20ddf9187ed2f96a1456dee
MitthrawnuruodoNull <34247396+MitthrawnuruodoNull@users.noreply.github.com>
2018-01-02 15:13:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ title = myfont.render("X-COM: UFO DEFENSE", 1, (black))
 #MORE SETTING UP
 display_width = 800
 display_height = 600
-scene = start
+scene = "start"
 screen = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('X-COM UFO DEFENSE')
 class Button(object):
@@ -25,7 +25,10 @@ class Button(object):
         text1 = myfont.render(str(self.text), 1, (black))
         pygame.draw.rect(screen,[255,255,255],[int(self.x),int(self.y),int(self.width),int(self.height)],0)
         screen.blit(text1, (self.x + self.width/3,self.y + self.height/3))
-    def click(self)
+        
+    def click(self):
+        if self.get_rect.collidepoint(pygame.mouse.get_pos()): 
+            print("hi")
 titlescreen = Button(300,300,200,100,"Start")
 #MAIN LOOP STARTS HERE
 done = False
