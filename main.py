b24b86a5bc33086b67e8230fd51facf0bb06daf6
MitthrawnuruodoNull <34247396+MitthrawnuruodoNull@users.noreply.github.com>
2018-01-03 15:10:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ title = myfont.render("X-COM: UFO DEFENSE", 1, (black))
 #MORE SETTING UP
 display_width = 800
 display_height = 600
+SIZE = 25
+Grid = []
 scene = "start"
 screen = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('X-COM UFO DEFENSE')
@@ -31,15 +33,27 @@ class Button(object):
         text1 = myfont.render(str(self.text), 1, (black))
         pygame.draw.rect(screen,[255,255,255],self.rect,0)
         screen.blit(text1, (self.x + self.width/3,self.y + self.height/3))
-        
     def click(self):
         #EASY CLICK DETECTION
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.touching = True
-            print(scene)
         else:
             self.touching = False
+#Creating GRID class
+class GridSquare(object):
+    def __init__(self, internal_row, internal_column, touching,clicked):
+        self.internal_row = internal_row
+        self.internal_column = internal_column
+        self.touching = touching
+        self.clicked = clicked
+        self.rect = pygame.Rect([int(self.internal_row)*10,int(self.internal_column)*10,25,25])
+    def draw(self):
+        pygame.draw.rect(screen,[255,0,0],self.rect,0)
 #BUTTONS
+for i in range(50,500,10):
+       Grid.append(GridSquare(100,100,False,False))
+       
+    
 titlescreen = Button(300,300,200,100,"Start",False,False)
 #MAIN LOOP STARTS HERE
 done = False
@@ -62,4 +76,7 @@ while done == False:
         scene = "game"
     if scene == "game":
         screen.fill(black)
+        for G in Grid:
+           G.draw()
+    
     pygame.display.flip()
